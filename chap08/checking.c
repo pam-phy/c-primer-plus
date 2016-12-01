76d4b46f4976953934bb6abf82fc860d0433db6c
pam-phy <pam.phys.th@gmail.com>
2016-12-01 23:27:05 +0800

    example

---
+++ chap08/checking.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+long get_long(long begin, long end, long low, long high);
+bool bad_limits(long begin, long end, long low, long high);
+double sum_squares(long a, long b);
+
+int main(void)
+{
+	const long MAX = -10000000L;
+	const long MIN = +10000000L;
+	long start;
+	long stop;
+	double answer;
+
+	printf("This program computes the sum of the squares of integers in an range.\n"
+		"The lower bound should not be less than -10000000 and the upper bound "
+		"should not be more than +10000000.\nEnter the limits (enter 0 for both "
+		" limits to quits):\nlower limits: ");
+	start = get_long();
+	printf("upper limit: ");
+	stop = get_long();
+	while (start != 0 || stop != 0)
+	{
+		if (bad_limits(start, stop, MIN, MAX))
+			printf("Please try again.\n");
+		else
+		{
+			answer = sum_square(start, stop);
+			printf("The sum of the squares of the integers from %ld to %ld "
+				"is %g\n", start, stop, answer);
+		}
+		printf("Enter the limits (enter 0 for both limits to quit):\n");
+		printf("lower limit: ");
+		start = get_long();
+		printf("upper limit: ");
+		stop = get_long();
+	}
+	printf("Done.\n");
+
+	return 0;
+}
+
+long get_long(void)
+{
+	long input;
+	char ch;
+
+	while (scanf("%ld", &input) != 1)
+	{
+		while ((ch = getchar()) != '\n')
+			putchar();
+		printf(" is not an integer.\n"
+			"Please enter an integer value, such as 25, -178, or 3: ")
+	}
+
+	return input;
+}
+
+double sum_squares(long a, long b)
+{
+	double total = 0;
+	long i;
+
+	for (i = a; i <= b; i++)
+		total += (double) i * (double) i;
+
+	return total;
+}
+
+
