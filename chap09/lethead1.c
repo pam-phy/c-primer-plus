c2851b3b951a296607788f508f6da4e1e59dc6a4
pam-phy <pam.phys.th@gmail.com>
2016-12-06 23:21:02 +0800

    exaple

--- chap09/lethead1.c
+++ chap09/lethead1.c
@@ -1,27 +1,28 @@
-#include<stdio.h>
+/* lethead1.c */
+#include <stdio.h>
 #define NAME "GIGATHINK, INC."
-#define ADRESS "101 Megabuck Plaza"
+#define ADDRESS "101 Megabuck Plaza"
 #define PLACE "Megapolis, CA 94904"
 #define WIDTH 40
 
-void starbar(void);	/* prototype the function */
+void starbar(void);  /* prototype the function */
 
 int main(void)
 {
-	starbar();
-	printf("%s\n", NAME);
-	printf("%s\n", ADRESS);
-	printf("%s\n", PLACE);
-	starbar();	/* use the function	*/
-
-	return 0;
+    starbar();
+    printf("%s\n", NAME);
+    printf("%s\n", ADDRESS);
+    printf("%s\n", PLACE);
+    starbar();       /* use the function       */
+    
+    return 0;
 }
 
-void starbar(void)	/* define the function	*/
+void starbar(void)   /* define the function    */
 {
-	int count;
-
-	for (count = 1; count <= WIDTH; count++)
-		putchar('*');
-	putchar('\n');
+    int count;
+    
+    for (count = 1; count <= WIDTH; count++)
+        putchar('*');
+    putchar('\n');
 }
--- chap09/lethead2.c
+++ chap09/lethead2.c
@@ -1,7 +1,8 @@
-#include<stdio.h>
-#include<string.h>
+/* lethead2.c */
+#include <stdio.h>
+#include <string.h>            /* for strlen() */
 #define NAME "GIGATHINK, INC."
-#define ADRESS "101 Megabuck Plaza"
+#define ADDRESS "101 Megabuck Plaza"
 #define PLACE "Megapolis, CA 94904"
 #define WIDTH 40
 #define SPACE ' '
@@ -10,27 +11,31 @@ void show_n_char(char ch, int num);
 
 int main(void)
 {
-	int spaces;
-
-	show_n_char('*', WIDTH);
-	putchar('\n');
-	show_n_char(SPACE, 12);
-	printf("%s\n", NAME);
-	spaces = (WIDTH - strlen(ADRESS)) / 2;
-	show_n_char(SPACE, spaces);
-	printf("%s\n", ADRESS);
-	show_n_char(SPACE, (WIDTH - strlen(PLACE)) / 2);
-	printf("%s\n", PLACE);
-	show_n_char('*', WIDTH);
-	putchar('\n');
-
-	return 0;
+    int spaces;
+    
+    show_n_char('*', WIDTH);   /* using constants as arguments */
+    putchar('\n');
+    show_n_char(SPACE, 12);    /* using constants as arguments */
+    printf("%s\n", NAME);
+    spaces = (WIDTH - strlen(ADDRESS)) / 2;
+    /* Let the program calculate    */
+    /* how many spaces to skip      */
+    show_n_char(SPACE, spaces);/* use a variable as argument   */
+    printf("%s\n", ADDRESS);
+    show_n_char(SPACE, (WIDTH - strlen(PLACE)) / 2);
+    /* an expression as argument    */
+    printf("%s\n", PLACE);
+    show_n_char('*', WIDTH);
+    putchar('\n');
+    
+    return 0;
 }
 
+/* show_n_char() definition */
 void show_n_char(char ch, int num)
 {
-	int count;
-
-	for (count = 1; count <= num; count++)
-		putchar(ch);
+    int count;
+    
+    for (count = 1; count <= num; count++)
+        putchar(ch);
 }
