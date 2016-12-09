84aa8388118eb0753e0f9e97da36e2cc3eed37f9
pam-phy <pam.phys.th@gmail.com>
2016-12-09 22:24:48 +0800

    exercise

--- chap10/10-02.c
+++ chap10/10-02.c
@@ -19,12 +19,12 @@ int main(void)
 
 	copy_ptr(target2, source, 5);
 	for (i = 0; i < 5; i++)
-		printf("%.1f ", target2 + i);
+		printf("%.1f ", *(target2 + i));
 	printf("\n");
 
 	copy_ptrs(target3, source, source + 5);
 	for (i = 0, index = source; index < source + 5; i++, index++)
-		printf("%.1f ", target3 + i);
+		printf("%.1f ", *(target3 + i));
 	printf("\n");
 
 	return 0;
@@ -36,8 +36,6 @@ void copy_arr(double target1[], double source[], int n)
 
 	for (i = 0; i < n; i++)
 		target1[i] = source[i];
-
-	return ;
 }
 
 void copy_ptr(double * target2, double * source, int n)
@@ -46,8 +44,6 @@ void copy_ptr(double * target2, double * source, int n)
 
 	for (i = 0; i < n; i++)
 		target2[i] = *(source + i);
-
-	return ;
 }
 
 void copy_ptrs(double * target3, double * source, double * end)
@@ -57,6 +53,4 @@ void copy_ptrs(double * target3, double * source, double * end)
 
 	for (i = 0, index = source; index < end; i++, index++)
 		target3[i] = *index;
-
-	return ;
 }
