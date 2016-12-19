5a62b9ef5facb156356fb7de390b4d1db5d6e938
pam-phy <pam.phys.th@gamil.com>
2016-12-19 23:14:55 +0800

    example

--- chap09/binary.c
+++ chap09/binary.c
@@ -4,9 +4,9 @@ void to_binary(unsigned long n);
 
 int main(void)
 {
-    unsigned long number;
-    printf("Enter an integer (q to quit):\n");
-    while (scanf("%lu", &number) == 1)
+	unsigned long number;
+	printf("Enter an integer (q to quit):\n");
+	while (scanf("%lu", &number) == 1)
     {
         printf("Binary equivalent: ");
         to_binary(number);
