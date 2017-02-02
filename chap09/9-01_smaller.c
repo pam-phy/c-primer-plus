372a2dd49fc7f188fdaa99400c51ee20c3a56941
pam-phy <pam.phys.th@gmail.com>
2017-02-03 00:37:23 +0800

    exercise ch09

--- chap09/9-01_smaller.c
+++ chap09/9-01_smaller.c
@@ -1,20 +1,18 @@
 #include<stdio.h>
-double dmin(double x, double y);
+double min(double x, double y);
 
 int main(void)
 {
-	printf("The smaller one of 12.3 and 68 is %f.\n", dmin(12.3, 68));
+	double x, y;
+
+	printf("Enter two type double values: ");
+	while (scanf("%lf %lf", &x, &y) == 2)
+		printf("The smaller one of %.2f and %.2f is %.2f.\n", x, y, min(x, y));
 
 	return 0;
 }
 
-double dmin(double x, double y)
+double min(double x, double y)
 {
-	double min;
-
-	min = x;
-	if (x > y)
-		min = y;
-
-	return min;
+	return ((x < y) ? x : y);
 }
