9243f618d21fa5fe7cce2d333f60a14850be5e06
pam-phy <pam.phys.th@gamil.com>
2017-02-14 16:56:20 +0800

    example

--- chap11/sort_str.c
+++ chap11/sort_str.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
 #include <string.h>
+#define SIZE 81			/* string length limit, including \0	*/
+#define LIM 20			/* maxinum number of lines to be read	*/
+#define HALT ""			/* null string to stop input			*/
 
+void stsrt(char * strings[], int num);	/* string-sort function	*/
 char * s_gets(char * st, int n);
+
+int main(void)
+{
+	char input[LIM][SIZE];		/* array to store input			*/
+	char * ptstr[LIM];			/* array of pointer variables	*/
+	int ct = 0;					/* input count					*/
+	int k;						/* output count					*/
+
+	printf("Input up to %d lines, and I will sort them.\n", LIM);
+	printf("To stop, press the Enter key at a line's start.\n");
+	while (ct < LIM && s_gets(input[ct], SIZE) != NULL
+			&& input[ct][0] != '\0')
+	{
+		ptstr[ct] = input[ct];	/* set ptrs to strings			*/
+		ct++;
+	}
+	stsrt(ptstr, ct);			/* string sorter				*/
+	puts("\nHere's the sorted lists:");
+	for (k = 0; k < ct; k++)
+		puts(ptstr[k]);			/* sorted pointers				*/
+
+	return 0;
+}
+
+/* string-pointer-sorting function	*/
+void stsrt(char * strings[], int num)
+{
+	char * temp;
+	int top, seek;
+
+	for (top = 0; top < num - 1; top++)
+		for (seek = top + 1; seek < num; seek++)
+			if (strcmp(strings[top], strings[seek]) > 0)
+			{
+				temp = strings[top];
+				strings[top] = strings[seek];
+				strings[seek] = temp;
+			}
+}
 
 char * s_gets(char * st, int n)
 {
