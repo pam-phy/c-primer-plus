0bf2386fe92689cdec21b588d25893367483c847
pam-phy <pam.phys.th@gamil.com>
2017-04-07 19:30:37 +0800

    example

---
+++ chap12/listing/partb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+extern int count;		// reference declaration, external linkage
+static int total = 0;	// static definition, internal linkage
+
+void accumulate(int k);	// prototype
+
+void accumulate(int k)	// k is block scope, no linkage
+{
+	static int subtotal = 0;	// static, no linkage
+
+	if (k <= 0)
+	{
+		printf("loop cycle: %d\n", count);
+		printf("subtotal: %d; total: %d\n", subtotal, total);
+		subtotal = 0;
+	}
+	else
+	{
+		subtotal += k;
+		total += k;
+	}
+}
